9a22104756476423569a7c4e1e0176bfcf18e06d
Dima2024-python <chagaydapython2024@gmail.com>
2024-09-07 21:43:52 +0300

    01_created_project

---
+++ database.py
@@ -0,0 +1,32 @@
+import config
+from sqlalchemy import Column, DateTime, Float, Integer, Text, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class Products(Base):
+    __tablename__ = 'Products'
+
+    id = Column(Integer, primary_key=True)
+    product_name = Column(Text, nullable=False)
+    price = Column(Float, nullable=False)
+    product_class = Column(Text, nullable=False)
+    start_product_expiration_date = Column(DateTime, nullable=False)
+    end_product_expiration_date = Column(DateTime, nullable=False)
+
+    def __str__(self):
+        return (f'Product ==> name:{self.product_name}; price: {self.price};'
+                f' product expiration date: {self.start_product_expiration_date} -> {self.end_product_expiration_date}')
+
+    __repr__ = __str__
+
+
+engine = create_engine(config.DB_PATH, echo=config.DEBUG)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def create_tables():
+    Base.metadata.create_all(engine)
---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from database import create_tables
+
+
+def lifespan(app: FastAPI):
+    create_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get('/')
+def index() -> dict:
+    return {'status': '200'}
