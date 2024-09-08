ffec3b826e81c833877c34321fbae7a4933dd68e
Dima2024-python <chagaydapython2024@gmail.com>
2024-09-08 21:26:07 +0300

    03_created_user

--- web/web_products.py
+++ web/web_products.py
@@ -1,20 +1,28 @@
-from fastapi import APIRouter
+from datetime import datetime
+
+from fastapi import APIRouter, Depends
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 
 import dao
+from utils.jwt_auth import get_user_web, set_cookies_web
 
 templates = Jinja2Templates(directory="templates")
 
-web_router = APIRouter(
+web_router_products = APIRouter(
     prefix="",
 )
 
 
-@web_router.get('/')
-def index(request: Request):
+@web_router_products.get('/', include_in_schema=True)
+@web_router_products.post('/', include_in_schema=True)
+def index(request: Request, user=Depends(get_user_web)):
     context = {
         'request': request,
         'products': dao.get_all_products(),
-        "title": "Main page"}
-    return templates.TemplateResponse("index.html", context=context)
+        'title': 'Main page',
+        'user': user
+    }
+    response = templates.TemplateResponse('index.html', context=context)
+    response_with_cookies = set_cookies_web(user, response)
+    return response_with_cookies
